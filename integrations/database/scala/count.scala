3cf6826c3792e0e782e32b59082ae68193d315fa
Isaac <tesla@outlook.com.au>
2022-01-25 07:05:02 +1100

    Refactoring of integrations.

--- integrations/database/scala/count.scala
+++
@@ -1,6 +0,0 @@
-spark-shell --master spark://node1:7077
-
-val logfile = sc.textFile("hdfs://node1:8020/log-dataset")
-val errors = logfile.filter(_.startsWith("ERROR"))
-val hdfs = errors.filter(_.containers("HDFS"))
-hdfs.count()
